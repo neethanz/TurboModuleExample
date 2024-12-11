6d790be96f67980b73ff36e4061b5017ed341ffc
Nz <Kalaineethan@gmail.com>
2024-12-11 09:25:16 +0530

    native bridging

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by NZ on 2024-12-10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.mm
@@ -0,0 +1,12 @@
+//
+//  RCTNativeLocalStorage.m
+//  TurboModuleExample
+//
+//  Created by NZ on 2024-12-10.
+//
+
+#import "RCTNativeLocalStorage.h"
+
+@implementation RCTNativeLocalStorage
+
+@end
